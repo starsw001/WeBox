6e2c3ec08b2397a8cabd926a186784c2cd8f2cc7
若海 <mail@anrip.com>
2014-08-30 21:57:51 +0800

    优化编译

--- php5/include/php/main/php_config.h
+++ php5/include/php/main/php_config.h
@@ -2149,7 +2149,7 @@
 #define PHP_BLOWFISH_CRYPT 1
 
 /* PHP build date */
-#define PHP_BUILD_DATE "2014-08-29"
+#define PHP_BUILD_DATE "2014-08-30"
 
 /* Define if your system has fork/vfork/CreateProcess */
 #define PHP_CAN_SUPPORT_PROC_OPEN 1
@@ -2227,7 +2227,7 @@
 #define PHP_STD_DES_CRYPT 1
 
 /* uname -a output */
-#define PHP_UNAME "Darwin anrip-mbpro.lan 13.3.0 Darwin Kernel Version 13.3.0: Tue Jun 3 21:27:35 PDT 2014; root:xnu-2422.110.17~1/RELEASE_X86_64 x86_64"
+#define PHP_UNAME "Darwin Anrip-MBPro.local 13.3.0 Darwin Kernel Version 13.3.0: Tue Jun 3 21:27:35 PDT 2014; root:xnu-2422.110.17~1/RELEASE_X86_64 x86_64"
 
 /* Whether PHP has to use its own crypt_r for blowfish, des and ext des */
 #define PHP_USE_PHP_CRYPT_R 1
